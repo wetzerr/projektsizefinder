40a4ed59457b96cdfffe603305379e69527cfd39
boehmpa <patricboehm4@gmail.com>
2023-10-26 19:20:36 +0200

    let's gro

---
+++ src/App.js
@@ -0,0 +1,281 @@
+import './App.css';
+import React, { useState } from 'react';
+import styles from './Geschlecht/frage1.module.css'
+import styles2 from './Größe/frage2.module.css'
+import styles3 from './Schultern/frage3.module.css'
+import styles4 from './Normalerweise/frage4.module.css'
+import styles5 from './Passform/frage5.module.css'
+import Slider from './Größe/Slider';
+import SliderPassform from './Passform/SliderPassform';
+import GrößenEmpfehlung  from './Empfehlung/GroessenEmpfehlung.js';
+import styles6 from './Empfehlung/empfehlung.module.css'
+
+function App() {
+
+  const [fragen, setFragen] = useState([
+    {
+      text: 'WÄHLE DEIN BIOLOGISCHES GESCHLECHT',
+      antworten: [
+        { text: 'Männlich', klasse: styles.antwortbuttonfrage11 },
+        { text: 'Weiblich', klasse: styles.antwortbuttonfrage12 },
+      ],
+    },
+    {
+      text: 'DEINE GRÖßE',
+      antwort: '',
+    },
+    {
+       text: 'DEINE SCHULTERN',
+      antworten: [
+        { text: 'schmal', klasse: styles2.antwortbuttonfrage3 },
+        { text: 'durchschnitt', klasse: styles2.antwortbuttonfrage3 },
+        { text: 'breit', klasse: styles2.antwortbuttonfrage3 },
+      ], // Hier wird die Antwort als Zahl gespeichert 
+    },
+    {
+      text: 'DIESE GRÖßE TRÄGST DU NORMALWEISE',
+      antworten: [
+        { text: 'XS', klasse: styles3.antwortbuttonfrage4},
+        { text: 'S', klasse: styles3.antwortbuttonfrage4 },
+        { text: 'M', klasse: styles3.antwortbuttonfrage4},
+        { text: 'L', klasse: styles3.antwortbuttonfrage4},
+        { text: 'XL', klasse: styles3.antwortbuttonfrage4},
+      ], // Hier wird die Antwort als Zahl gespeichert
+    },
+    {
+      text: 'DIESE PASSFORM BEVORZUGST DU',
+      antworten: [],
+    },
+    {
+      text: 'EMPFOHLENE GRÖßE',
+      antworten: [],
+    },
+  ]);
+
+  const [ausgewählteAntworten, setAusgewählteAntworten] = useState(Array(fragen.length).fill(null));
+  const [frageIndex, setFrageIndex] = useState(0);
+  const [quizAngezeigt, setQuizAngezeigt] = useState(false);
+  const [größe, setGröße] = useState(null); // Hier wird die Größe gespeichert// Hier Slider-Wert initialisieren
+  const [sliderPassValue, setSliderPassValue] = useState(2);
+  const [größe2, setGröße2 ] = useState(null);
+  const [selectedUnit, setSelectedUnit] = useState("cm");
+  
+   // Funktion zum Umschalten der Einheit zwischen "cm" und "ftin"
+   const handleUnitChange = () => {
+    setSelectedUnit(selectedUnit === "cm" ? "ftin" : "cm");
+  };
+  
+  const handleTextKlick = () => {
+    // Wenn der Text geklickt wird, zeige das Quiz an
+    setQuizAngezeigt(true);
+    setFrageIndex(0);
+  };
+
+
+  const nächsteFrage = () => {
+    // Füge Logik hinzu, um zur nächsten Frage zu wechseln
+  
+    // Bedingung für das Setzen von größe2 (Slider-Wert für Passform)
+    if (frageIndex === 4) {
+      let passformValue = '';
+      
+      switch (sliderPassValue) {
+        case '0':
+          passformValue = 'Locker';
+          break;
+        case '1':
+          passformValue = 'Etwas Lockerer';
+          break;
+        case '2':
+          passformValue = 'Regulär';
+          break;
+        case '3':
+          passformValue = 'Etwas Enger';
+          break;
+        case '4':
+          passformValue = 'Figurbetont';
+          break;
+        default:
+          passformValue = sliderPassValue;
+          break;
+      }
+      
+      setGröße2(passformValue);
+    }
+    
+    
+     // Füge ein Delay von 1000 Millisekunden (1 Sekunde) hinzu
+    setTimeout(() => {
+    // Erhöhe immer den Index, es sei denn, du hast spezielle Bedingungen
+    setFrageIndex(frageIndex + 1);
+    }, 200);
+  };
+  
+  const zurückFrage = () => {
+    if (frageIndex < fragen.length) {
+      setFrageIndex(frageIndex - 1);
+    } else {
+
+    }
+  };
+
+  const handleAntwortKlick = (antwort) => {
+    const neueAusgewählteAntworten = [...ausgewählteAntworten];
+    neueAusgewählteAntworten[frageIndex] = antwort;
+    setAusgewählteAntworten(neueAusgewählteAntworten);
+    if (frageIndex + 1< fragen.length) {
+      setFrageIndex(frageIndex + 1);
+    }
+  };
+
+  const handleErsteFrageAntwort = (antwort) => {
+    const neueAusgewählteAntworten = [...ausgewählteAntworten];
+    neueAusgewählteAntworten[0] = antwort;
+  
+    setAusgewählteAntworten(neueAusgewählteAntworten);
+  
+    nächsteFrage();
+  };
+  
+  const handleDritteFrageAntwort = (antwort) => {
+  
+    const neueAusgewählteAntworten = [...ausgewählteAntworten];
+    neueAusgewählteAntworten[2] = antwort;
+  
+    setAusgewählteAntworten(neueAusgewählteAntworten);
+  
+    nächsteFrage();
+  };
+
+  const handleVierteFrageAntwort = (antwort) => {
+    
+    const neueAusgewählteAntworten = [...ausgewählteAntworten]; // Erstelle eine Kopie des aktuellen Zustands
+    neueAusgewählteAntworten[3] = antwort; // Speichere die ausgewählte Antwort in ausgewählteAntworten[3]
+    
+    setAusgewählteAntworten(neueAusgewählteAntworten); // Aktualisiere den Zustand mit der neuen Antwort
+  
+    nächsteFrage(); // Gehe zur nächsten Frage
+  };
+
+  const handleSliderChange1 = (newValue) => {
+    setSliderPassValue(newValue);
+    // Hier können Sie die Logik hinzufügen, die Sie beim Ändern des Sliders ausführen möchten
+  };
+
+  const closeQuiz = () => {
+    const quizContainer = document.querySelector('.quiz-container');
+    quizContainer.style.display = 'none';
+    setQuizAngezeigt(false);
+  };
+
+  const restartQuiz = () => {
+    setFrageIndex(0);
+  }
+  
+
+  return (
+   <>
+      {!quizAngezeigt && (
+        <div>
+          <div className='io'></div>
+          <button className="start-text" onClick={handleTextKlick}>DEINE PASSENDE GRÖßE</button>
+          <img src='/images/Kleiderbuegel.png' className='kleiderbuegel' onClick={handleTextKlick}></img>
+        </div>
+      )}
+      {quizAngezeigt && (  
+          <div className='quiz-container'>
+            {frageIndex < fragen.length && (
+              <div>
+                {frageIndex > 0 && frageIndex < 5 && (
+                  <div className='closex-backarrow'>
+                    <img src='/images/back-arrow.webp' id='backarrow' onClick={zurückFrage} alt='zurück'></img>
+                    <img src='/images/closeX.webp' id='closex' onClick={closeQuiz} alt='close'></img>
+                  </div>
+                )}
+                {frageIndex === 0 && (
+                    <> 
+                        <div className={styles.divider}>
+                          <img src='/images/closeX.webp' className={styles.closex} onClick={closeQuiz} alt='close'></img>
+                          <h1 className={styles.h1}>WIR HELFEN DIR DEINE PASSENDE GRÖßE ZU FINDEN</h1>
+                          <p className={styles.line}></p>
+                        </div>
+                        <h2 className={styles.frage1}>DEIN BIOLOGISCHES GESCHLECHT</h2>
+                        <button className={styles.antwortbuttonfrage11} onClick={() => handleErsteFrageAntwort('Männlich')}>Männlich</button>
+                        <button className={styles.antwortbuttonfrage12} onClick={() => handleErsteFrageAntwort('Weiblich')}>Weiblich</button>
+                        <p className={styles.poweredby}>Powered by <span className={styles.span}>boehm.IO</span></p>
+                    </>
+                )}
+                {frageIndex === 1 && (
+                  <div>
+                        <h2 className={styles2.frage2}>DEINE KÖRPERGRÖßE</h2>
+                        <div className={styles2.ContainerSwitchEinheiten}>
+                          <label className={styles2.switch}>
+                            <span className={styles2.einheitenCm}>cm</span>
+                            <input 
+                              type= "checkbox"
+                              checked={selectedUnit === "ftin"}
+                              onChange={handleUnitChange}
+                            />                          
+                            <span className={styles2.sliderEinheit}></span>
+                            <span className={styles2.einheitenFtin}>ft in</span>
+                          </label>
+                        </div>
+                    <Slider value={größe} onSliderChange={(newValue) => setGröße(newValue)} unit={selectedUnit}/>
+                    <button className={styles2.nächstefragebutton} onClick={nächsteFrage}>WEITER</button>
+                    <p className={styles2.poweredby}>Powered by <span className={styles2.span}>boehm.IO</span></p>
+                  </div>
+                )}
+                {frageIndex === 2 && (
+                  <div>
+                      <h1 className = {styles3.frage3}>DEINE SCHULTERN</h1>
+                      <img src='/images/schmal.png' className= {styles3.schulternbild} alt='schmal' onClick={() => handleDritteFrageAntwort('schmal')}></img>
+                      <img src='/images/durchschnitt.png' className= {styles3.schulternbild} alt='durchschnitt'  onClick={() => handleDritteFrageAntwort('durchschnitt')}></img>
+                      <img src='images/breit.png' className={styles3.schulternbild} alt='breit-bild'  onClick={() => handleDritteFrageAntwort('breit')}></img>
+                      <button className={styles3.antwortbuttonfrage3} onClick={() => handleDritteFrageAntwort('schmal')}>schmal</button>
+                      <button className= {styles3.antwortbuttonfrage3} onClick={() => handleDritteFrageAntwort('durchschnitt')}>durchschnitt</button>
+                      <button className ={styles3.antwortbuttonfrage3} onClick={() => handleDritteFrageAntwort('breit')}>breit</button>
+                      <p className={styles3.poweredby}>Powered by <span className={styles3.span}>boehm.IO</span></p>
+                    </div>       
+                )}
+                {frageIndex === 3 && (
+                  <div>
+                    <h1 className= {styles4.frage4}>DIESE GRÖßE TRÄGST DU NORMALWEISE</h1>
+                    <button className={styles4.antwortbuttonfrage4} onClick= {() => handleVierteFrageAntwort('XS')}>XS</button>
+                    <button className={styles4.antwortbuttonfrage4} onClick= {() => handleVierteFrageAntwort('S')}>S</button>
+                    <button className={styles4.antwortbuttonfrage4} onClick= {() => handleVierteFrageAntwort('M')}>M</button>
+                    <button className={styles4.antwortbuttonfrage4} onClick= {() => handleVierteFrageAntwort('L')}>L</button>
+                    <button className={styles4.antwortbuttonfrage4} onClick= {() => handleVierteFrageAntwort('XL')}>XL</button>          
+                    <h2 className= {styles4.frage45}>TRÄGST DU IMMER DIESE GRÖßE</h2>
+                    <p className={styles4.poweredby}>Powered by <span className={styles4.span}>boehm.IO</span></p>  
+                  </div>
+                )}
+                {frageIndex === 4 && (
+                    <div>
+                      <h1 className={styles5.frage5}>DIESE PASSFORM BEVORZUGST DU</h1>
+                      <button className={styles5.nächstefragebuttonSlider2} onClick={nächsteFrage}>WEITER</button>
+                      <SliderPassform value={größe2} onChange={handleSliderChange1}/>                     
+                      <p className={styles5.poweredby}>Powered by <span className={styles5.span}>boehm.IO</span></p>
+                    </div>
+                )}
+                {frageIndex === 5 && (
+                    <div>
+                      <img src='/images/closeX.webp' className={` ${styles6.closex} ${styles5.alignBottom} `} onClick = {closeQuiz} alt='close'></img>
+                      <GrößenEmpfehlung 
+                        sliderValue1={größe}
+                        sliderValue2={größe2}
+                        ausgewählteAntworten={ausgewählteAntworten}
+                      />
+                      <button className={styles6.quizSchliessen} onClick={closeQuiz}>QUIZ SCHLIESSSEN</button>
+                      <button className={styles6.vonVorne} onClick={restartQuiz}>VON VORNE BEGINNEN</button>
+                      <p className={styles6.poweredby}>Powered by <span className={styles6.span}>boehm.IO</span></p>
+                    </div>
+                )}
+              </div>
+            )}
+          </div>
+      )}
+    </>
+  );
+}  
+
+export default App;
---
+++ src/Empfehlung/GroessenEmpfehlung.js
@@ -0,0 +1,61 @@
+import styles from './empfehlung.module.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function GrößenEmpfehlung({ ausgewählteAntworten, sliderValue1, sliderValue2 }) {
+    const geschlecht = ausgewählteAntworten[0];
+    const schultern = ausgewählteAntworten[2];
+    const größeNormal = ausgewählteAntworten[3]; // Beispielantwort, ersetze sie durch die tatsächliche Antwort
+    const heightValue = sliderValue1;
+    const passform = sliderValue2;
+
+
+    console.log(geschlecht);
+    console.log("Größe :", heightValue);
+    console.log(schultern);
+    console.log(größeNormal);
+    console.log(passform);
+
+    const speichereAntwortenInDatenbank = () => {
+      const antworten = {
+        geschlecht,
+        schultern,
+        größeNormal,
+        heightValue,
+        passform,
+        empfohleneGröße,
+      };
+      axios.post('/api/speichereAntworten', antworten)
+      .then(response => {
+        console.log('Antworten erfolgreich in der Datenbank gespeichert.');
+      })
+      .catch(error => {
+        console.error('Fehler beim Speichern der Antworten: ', error);
+      });
+  };
+
+    // Logik für die empfohlene Größe basierend auf den Antworten
+
+    let empfohleneGröße = '';
+
+    if (geschlecht === 'Männlich' && heightValue < '170' && schultern === 'schmal' && größeNormal === 'XS' &&  passform === 'Figurbetont') 
+    {
+      empfohleneGröße = 'S';
+    }
+    else if (geschlecht === 'Männlich' && heightValue >= '170' && schultern === 'durchschnitt' && größeNormal === 'M' &&  passform === 'Etwas Lockerer')
+    {
+      empfohleneGröße = 'M';
+    }
+    else {
+      empfohleneGröße = 'Keine Empfehlung';
+    }
+
+    return (
+        <div className={styles.größenempfehlung}>
+            <h3 className={styles.empfG}>Empfohlene Größe:</h3>
+            <p>{empfohleneGröße}</p>
+        </div>
+    );
+}
+
+export default GrößenEmpfehlung;
